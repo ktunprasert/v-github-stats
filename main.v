7b5287f6e0fdb86b331a32fcbe1a24aca191ee75
ktunprasert <ktunprasert@outlook.com>
2025-07-06 22:31:35 +0100

    feat: graphql template file

--- main.v
+++ main.v
@@ -1,5 +1,15 @@
 module main
 
+import os
+import net.http
+import zztkm.vdotenv
+
+const base_url := "https://api.github.com/graphql"
+
 fn main() {
-	println('Hello World!')
+	user := 'ktunprasert'
+	vdotenv.load()
+	pat := os.getenv('TOKEN')
+
+	println($tmpl('./graphql/languages.graphql'))
 }

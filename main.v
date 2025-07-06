ab4e70a82d9d45ac017d7e8af2cca21fa8a80989
ktunprasert <ktunprasert@outlook.com>
2025-07-06 22:39:52 +0100

    feat: minimum working version

--- main.v
+++ main.v
@@ -3,13 +3,28 @@ module main
 import os
 import net.http
 import zztkm.vdotenv
+import json
 
-const base_url := "https://api.github.com/graphql"
+const base_url = 'https://api.github.com/graphql'
 
 fn main() {
 	user := 'ktunprasert'
 	vdotenv.load()
 	pat := os.getenv('TOKEN')
 
-	println($tmpl('./graphql/languages.graphql'))
+	graphql := $tmpl('./graphql/languages.graphql')
+	println(graphql)
+
+	body_map := {
+		'query': graphql
+	}
+	body := json.encode(body_map)
+	println(body)
+
+	mut request := http.new_request(.post, base_url, body)
+	request.add_header(.authorization, 'Bearer ${pat}')
+	println(request.header)
+
+	response := request.do()!
+	println(response)
 }

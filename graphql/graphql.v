627e5e382683790dbd4d6095bc7324d76bcd06aa
ktunprasert <ktunprasert@outlook.com>
2025-07-07 15:52:35 +0100

    feat(graphql): use @params tag to signify optional

--- graphql/graphql.v
+++ graphql/graphql.v
@@ -5,14 +5,26 @@ import json
 
 const gql_lang = 'languages.graphql'
 
-pub fn new_language() Languages {
-	return Languages{}
-}
-
-struct Languages {
+@[params]
+pub struct LanguagesConfig {
+pub:
 	user          string = os.getenv('GH_USER')
 	num_repos     int    = 5
 	num_languages int    = 10
+}
+
+pub fn new_language(l LanguagesConfig) Languages {
+	return Languages{
+		user:          l.user
+		num_repos:     l.num_repos
+		num_languages: l.num_languages
+	}
+}
+
+struct Languages {
+	user          string
+	num_repos     int
+	num_languages int
 }
 
 fn (l Languages) query() string {

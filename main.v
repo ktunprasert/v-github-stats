233616d78cb96ff3f2f3d47d65c685cdd456ef38
ktunprasert <ktunprasert@outlook.com>
2025-07-07 21:18:18 +0100

    feat(main): comptime color && print color per lang

--- main.v
+++ main.v
@@ -5,6 +5,7 @@ import zztkm.vdotenv
 import graphql
 import client
 import log
+import json
 
 struct Config {
 mut:
@@ -12,11 +13,25 @@ mut:
 	token string = os.getenv('GH_TOKEN')
 }
 
+struct Color {
+	color string
+	url   string
+}
+
+const cmap = json.decode(map[string]Color, $embed_file('colors.json').to_string())!
+
 fn main() {
 	vdotenv.load()
 	log.set_level(.debug)
-	// cfg := Config{}
+
 	c := client.new_client()
-	response := c.query[client.LanguagesResponseDTO](graphql.new_language(num_repos: 100))!
-	log.info(response.get_languages().str())
+	response := c.query[client.LanguagesResponseDTO](graphql.new_language(num_repos: 1))!
+	languages := response.get_languages()
+	log.info(languages.str())
+
+	for key, _ in languages {
+		println('lang: $key')
+		clr := cmap[key]
+		println('color: $clr')
+	}
 }

9a646674f149e622d4f8c0d3c17117d12eb489ad
ktunprasert <ktunprasert@outlook.com>
2025-07-07 18:41:17 +0100

    feat(main): use new modules

--- main.v
+++ main.v
@@ -1,12 +1,9 @@
 module main
 
 import os
-import net.http
 import zztkm.vdotenv
-import json
 import graphql
-
-const base_url = 'https://api.github.com/graphql'
+import client
 
 struct Config {
 mut:
@@ -14,64 +11,12 @@ mut:
 	token string = os.getenv('GH_TOKEN')
 }
 
-// as of 2025-07-06T22:59:57 the anonymous struct requires @json attribute
-struct LanguagesResponseDTO {
-	data struct {
-		user struct {
-			respositories struct {
-				nodes []struct {
-					name      string
-					pushed_at string @[json: 'pushedAt']
-					languages struct {
-						edges []struct {
-							size int
-							node struct {
-								name string
-							}
-						}
-					}
-				}
-			} @[json: 'repositories']
-		} @[json: 'user']
-	} @[json: 'data']
-}
-
-fn (dto LanguagesResponseDTO) get_languages() map[string]int {
-	mut languages := map[string]int{}
-	for repo in dto.data.user.respositories.nodes {
-		for edge in repo.languages.edges {
-			language := edge.node.name
-			size := edge.size
-			if language in languages {
-				languages[language] += size
-			} else {
-				languages[language] = size
-			}
-		}
-	}
-
-	return languages
-}
-
 fn main() {
 	vdotenv.load()
-	cfg := Config{}
-	body := graphql.new_language().to_body()
+	// cfg := Config{}
+	c := client.new_client()
+	response := c.query[client.LanguagesResponseDTO](graphql.new_language())!
 
-	mut request := http.new_request(.post, base_url, body)
-	request.add_header(.authorization, 'Bearer ${cfg.token}')
-	println(request.header)
-
-	response := request.do()!
-	println(response.status_code)
-
-	if response.status_code > 200 {
-		eprintln('something went wrong - check your environment values or .env')
-		exit(1)
-	}
-
-	dto := json.decode(LanguagesResponseDTO, response.body)!
-	println(dto)
-
-	println(dto.get_languages())
+	println(response)
+	println(response.get_languages())
 }
